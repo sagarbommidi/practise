0f3515a841e13e73a1e5808da78bbcbf1fa91e04
Sagar Bommidi <sagar.bommidi@gmail.com>
2016-12-16 21:28:43 +0530

    Added first element as pivot element in quick sort

--- ds_and_algos/sortings/quick_sort.rb
+++ ds_and_algos/sortings/quick_sort.rb
@@ -1,10 +1,11 @@
 def quick_sort(arr, findex, lindex)
   return if findex >= lindex
-  pivot_index = partition(arr, findex, lindex)
+  pivot_index = partition1(arr, findex, lindex)
   quick_sort(arr, findex, pivot_index-1)
   quick_sort(arr, pivot_index+1, lindex)
 end
 
+# The below partition is based on the PIVOT element as last element in sub array
 def partition(arr, left, right)
   pivot = arr[right]
   pindex = left
@@ -18,6 +19,21 @@ def partition(arr, left, right)
   pindex
 end
 
+# The below partition is based on the PIVOT element as first element in sub array
+def partition1(arr, left, right)
+  pele = arr[left]
+  pindex = right
+  i = right
+  while(i > left)
+    if(arr[i] > pele)
+      arr[i], arr[pindex] = arr[pindex], arr[i]
+      pindex -= 1
+    end
+    i -= 1
+  end
+  arr[left], arr[pindex] = arr[pindex], arr[left]
+  pindex
+end
 
 a = [1, 4, 6, 9, 2, 5, 7, 3, 8]
 quick_sort(a, 0, a.length-1)
